3cbb078f67d9ce2330631f79e7b7a725b86176c0
banu-riya <88086465+banu-riya@users.noreply.github.com>
2022-01-28 11:00:54 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,17 +2,17 @@ const BIAPreSale= artifacts.require("BIAPreSale");
 
 module.exports = function (deployer) {
   
-  deployer.deploy(BIAPreSale,'0xF2Dec16aD61B58c5b914419548747c72eF451Dc5','0xbe050b2e55a4d3002d215bc90528c7711a3fd7e0','46893317702227400000000','1643673601','1651363201','1656633601','1672531201','1704067201');
+  deployer.deploy(BIAPreSale,'4265','0xF2Dec16aD61B58c5b914419548747c72eF451Dc5','0xbe050b2e55a4d3002d215bc90528c7711a3fd7e0','46893317702227400000000','1643673601','1651363201','1656633601','1672531201','1704067201');
 
 };
 
 /*rate -> 4265
-wallet -> 0xF2Dec16aD61B58c5b914419548747c72eF451Dc5 (for Test)
-Token Address -> 0xbe050b2e55a4d3002d215bc90528c7711a3fd7e0 (for Test)
-CAP -> 46893317702227400000000  (BNB value of 200000000 BIA Token as given rate)   
-OpeningTime -> February 1, 2022 12:00:01 AM - 1643673601 ( This value will assigned based on deployment time))
-ClosingTime -> May 1, 2022 12:00:01 AM - 1651363201 ( Three Months from opening date)
-ReleaseTime1 -> July 1, 2022 12:00:01 AM - 1656633601
-ReleaseTime2 ->January 1, 2023 12:00:01 AM - 1672531201
-ReleaseTime3 ->January 1, 2024 12:00:01 AM - 1704067201
+wallet -> 0xF2Dec16aD61B58c5b914419548747c72eF451Dc5 (testnet address)
+Token Address -> 0xbe050b2e55a4d3002d215bc90528c7711a3fd7e0 (testnet token contract address)
+CAP -> 46893317702227400000000  (BNB(in wei) value for 200000000 BIA Tokens as per given rate)   
+OpeningTime -> February 1, 2022 00:00:01 AM - 1643673601 ( Opening value will be set during deployment time))
+ClosingTime -> May 1, 2022 00:00:01 AM - 1651363201 ( Three Months from opening date - will be set during deployment time)
+ReleaseTime1 -> July 1, 2022 00:00:01 AM - 1656633601 
+ReleaseTime2 ->January 1, 2023 00:00:01 AM - 1672531201
+ReleaseTime3 ->January 1, 2024 00:00:01 AM - 1704067201
 */
